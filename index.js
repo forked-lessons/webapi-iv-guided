d8baa073950abf9ba29f87b99ac8e17f9504dd7e
Daniel <danielstarling4@gmail.com>
2019-03-21 10:23:27 -0700

    update it more

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const server = require("./api/server.js");
 require("dotenv").config();
+
 const port = process.env.PORT || 4000;
-const greeting = process.env.GREETING;
+const greeting = process.env.GREETING || "Hello";
+
 server.listen(4000, () => {
   console.log(`\n*** ${greeting} http://localhost:${port} ***\n`);
 });
